c5aad3f84db626c021d3d3f787c093a19718b8b7
Lyp <l1yp@qq.com>
2022-11-19 09:11:35 +0800

    java最低支持8

--- src/main/java/com/l1yp/mapper/modeling/ModelingViewMapper.java
+++ src/main/java/com/l1yp/mapper/modeling/ModelingViewMapper.java
@@ -22,8 +22,8 @@ public interface ModelingViewMapper extends BaseMapper<ModelingView> {
     @Select("${sql}")
     @Results({
             @Result(property = "id", column = "id", jdbcType = JdbcType.BIGINT, javaType = String.class),
-            @Result(property = "updateBy", column = "update_by", jdbcType = JdbcType.BIGINT, javaType = String.class),
-            @Result(property = "createBy", column = "create_by", jdbcType = JdbcType.BIGINT, javaType = String.class),
+            @Result(property = "update_by", column = "update_by", jdbcType = JdbcType.BIGINT, javaType = String.class),
+            @Result(property = "create_by", column = "create_by", jdbcType = JdbcType.BIGINT, javaType = String.class),
     })
     List<Map<String, Object>> pageEntity(@Param("sql") String sql,
                          @Param("args") List<Object> args);
